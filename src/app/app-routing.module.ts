f481c1f95893d3f7fce8445a000f74073e90de6d
RobertoEsposito02 <robiespo02@libero.it>
2022-12-18 22:55:04 +0100

    modificato route con guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
   {
     path:'welcome',
-    loadChildren: () => import("./feature/welcome/welcome.module").then(m => m.WelcomeModule)
+    loadChildren: () => import("./feature/welcome/welcome.module").then(m => m.WelcomeModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'tavolo',
-    loadChildren: () => import("./feature/tavolo/tavolo.module").then(m => m.TavoloModule)
+    loadChildren: () => import("./feature/tavolo/tavolo.module").then(m => m.TavoloModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'auth',
-    loadChildren: () => import("./core/auth/auth.module").then(m => m.AuthModule)
+    loadChildren: () => import("./core/auth/auth.module").then(m => m.AuthModule),
   },
   {path:'',redirectTo:'welcome',pathMatch:'full'},
   {path: '**',redirectTo: 'welcome',pathMatch: 'full'}
